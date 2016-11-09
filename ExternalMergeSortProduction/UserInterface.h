a341eb9349943225642b43f35f8030b0ee8c5744
Alexander Filippov <aleksander.fill@gmail.com>
2016-11-10 00:12:57 +0300

    CD

--- ExternalMergeSortProduction/UserInterface.h
+++ ExternalMergeSortProduction/UserInterface.h
@@ -7,13 +7,14 @@ class UserInterface
 private:
 	ExternalMergeSort *sort;
 	const string menu = "0. Выход\n1. Сгенерировать последовательность\n2. Задать параметры сортировки\n3. Отсоритровать последовательность\n4. Оценить характеристики сортировки\n5. Получение зависимостей\n";
+	const char *responceString[9] = { "Успешно", "Ошибка генерации", "Файл не существует", "Ошибка размера", "Ошибка файл-менеджера",
+		"Исходный файл и файл результата совпадают", "Достигнут конец файла", "Ошибка выделения памяти", "Параметры не заданы или заданы неверно" };
 	bool callMethod(int ch);
 	void callGenerate();
 	void callSetParams();
 	void callSort();
 	void callEstimate();
 	void callGetDependencies();
-	char **responceString;
 	Responce setParams(FileManager *file, long long sizeOfSegments, TypeOfSort type);
 	void getDependenceSize();
 	void getDependenceSizeOfSegments();
